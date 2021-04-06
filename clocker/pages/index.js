bce3d8652591eb426c235117715e95e4eab65794
WeslleyFratini <weeslleey354@gmail.com>
2021-04-06 08:23:49 -0300

    identação

--- clocker/pages/index.js
+++ clocker/pages/index.js
@@ -1,49 +1,47 @@
-
-  import { useFormik } from 'formik'
-  import * as yup from 'yup'
-import { Logo } from './../components' 
-import { 
-  Container, 
-  Box, 
-  Input, 
-  Button, 
-  Text,FormControl, 
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { Logo } from "./../components";
+import {
+  Container,
+  Box,
+  Input,
+  Button,
+  Text,
+  FormControl,
   FormLabel,
-  FormHelperText} from '@chakra-ui/react'
+  FormHelperText,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
-   <Container p={4} centerContent>
-     <Logo/>
-     <Box p={4} mt={8}>
-       <Text>Crie sua agenda compartilhada</Text>
-     </Box>
-     
-     <Box>
+    <Container p={4} centerContent>
+      <Logo />
+      <Box p={4} mt={8}>
+        <Text>Crie sua agenda compartilhada</Text>
+      </Box>
 
+      <Box>
+        <FormControl id="email" p={4} isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" />
+        </FormControl>
 
-     <FormControl id="email" p={4} isRequired>
-  <FormLabel>Email</FormLabel>
-  <Input type="email" /> 
-</FormControl>
+        <FormControl id="password" p={4} isRequired>
+          <FormLabel>Senha</FormLabel>
+          <Input type="password" />
+        </FormControl>
 
-<FormControl id="password" p={4} isRequired>
-  <FormLabel>Senha</FormLabel>
-  <Input type="password" /> 
-</FormControl>
+        <Box display="flex" flexDirection="row" alignItems="center">
+          <Text>clocker.work/</Text>
+          <FormControl id="username" p={4} isRequired>
+            <Input type="username" />
+          </FormControl>
+        </Box>
 
-<Box display="flex" flexDirection="row" alignItems="center">
-  <Text>clocker.work/</Text>
-<FormControl id="username" p={4} isRequired>  
-  <Input type="username" /> 
-</FormControl>
-</Box>
-
-<Box p={4}>
-
-<Button width="100%">Entrar</Button>
-</Box>
-</Box>
+        <Box p={4}>
+          <Button width="100%">Entrar</Button>
+        </Box>
+      </Box>
     </Container>
-  )
+  );
 }
